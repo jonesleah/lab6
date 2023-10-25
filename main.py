bd0b7e2432359c182e1fdbf4ae872b1a0e3862dd
Leah Jones <leahjones1403@gmail.com>
2023-10-25 13:52:18 -0400

    encode function

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-# this is my first github addition
-print("Hello world")
+# Leah Jones and Justin Oh
+def encode(password):
+    encoded_string = ""
+    for digit in password:
+        if digit == "7":
+            encoded_string += "0"
+        if digit == "8":
+            encoded_string += "1"
+        if digit == "9":
+            encoded_string += "2"
+        encoded_string += str(int(digit) + 3)
+    return encoded_string
+
+
+
