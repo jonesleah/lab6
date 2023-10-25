cdac3027289b2c6f53943b5226cd2df6315de045
justinohh1227 <justinoh1227@gmail.com>
2023-10-25 14:16:14 -0400

    encode/decode completed project

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 # Leah Jones and Justin Oh
+# Function for the menu display
+def menu_display():
+    print("\nMenu")
+    print("-" * 13)
+
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+
+# Function to decode the input password
 def encode(password):
     encoded_string = ""
     for digit in password:
         if digit == "7":
             encoded_string += "0"
-        if digit == "8":
+        elif digit == "8":
             encoded_string += "1"
-        if digit == "9":
+        elif digit == "9":
             encoded_string += "2"
-        encoded_string += str(int(digit) + 3)
+        else:
+            encoded_string += str(int(digit) + 3)
     return encoded_string
 
+# Have user input their password to be decoded in a loop
+def main():
+    while True:
+        menu_display()
+
+        user_opt = input("\nPlease enter an option: ")
+
+        if user_opt == "1":
+            my_pass = input("Please enter your password to encode: ")
+            encoded_pass = encode(my_pass)
+            print("Your password has been encoded and stored!")
+        elif user_opt == "2":
+            print("The encoded password is " + encoded_pass + " and the original password is " + my_pass)
+        elif user_opt == '3':
+            break
+
+
+if __name__ == '__main__':
+    main()
 
 
